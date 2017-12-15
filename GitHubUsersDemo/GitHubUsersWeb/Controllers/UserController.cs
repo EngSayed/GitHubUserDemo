9bccab39060417aa2a4147d2f5ce14bdcb94bcac
Sayed <Sayed@S>
2017-12-15 10:25:06 +0100

    Add sorting mechanism

--- GitHubUsersDemo/GitHubUsersWeb/Controllers/UserController.cs
+++ GitHubUsersDemo/GitHubUsersWeb/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using AutoMapper;
 using GitHubApi;
@@ -28,6 +30,8 @@ namespace GitHubUsersWeb.Controllers
 
                 var mappedUser = mapper.Map<User, UserViewModel>(user);
 
+                TempData["User"] = mappedUser;
+
                 return View("Index", mappedUser);
             }
             catch (ApplicationExceptionBase exception)
@@ -40,6 +44,74 @@ namespace GitHubUsersWeb.Controllers
                 ModelState.AddModelError("ErrorMessage", exception.Message);
                 return View();
             }
+        }
+                
+        public ActionResult Sort(string sortOrder)
+        {
+            var userModel = TempData["User"] as UserViewModel;
+            if (userModel == null)
+                return View("Index");
+
+
+            var repos = userModel.Repositories;
+            var userReposList = repos as IList<UserReporViewModel> ?? repos.ToList();
+            if (!userReposList.Any())
+                return View("Index");
+
+            TempData.Keep("User");
+
+            ViewBag.NameSortParm     = sortOrder == "name" ? "name_desc" : "name";
+            ViewBag.FullNameSortParm = sortOrder == "full_name" ? "full_name_desc" : "full_name";
+            ViewBag.StarsSortParm    = sortOrder == "stars" ? "stars_desc" : "stars";
+            ViewBag.LanguageSortParm = sortOrder == "language" ? "language_desc" : "language";
+            ViewBag.DescrSortParm    = sortOrder == "description" ? "description_desc" : "description";
+
+            switch (sortOrder)
+            {
+                case "name":
+                    repos = userReposList.OrderBy(s => s.Name);
+                    userModel.Repositories = repos;
+                    break;
+                case "name_desc":
+                    repos = userReposList.OrderByDescending(s => s.Name);
+                    userModel.Repositories = repos;
+                    break;
+                case "full_name":
+                    repos = userReposList.OrderBy(s => s.FullName);
+                    userModel.Repositories = repos;
+                    break;
+                case "full_name_desc":
+                    repos = userReposList.OrderByDescending(s => s.FullName);
+                    userModel.Repositories = repos;
+                    break;
+                case "language":
+                    repos = userReposList.OrderBy(s => s.Language);
+                    userModel.Repositories = repos;
+                    break;
+                case "language_desc":
+                    repos = userReposList.OrderByDescending(s => s.Language);
+                    userModel.Repositories = repos;
+                    break;
+                case "description":
+                    repos = userReposList.OrderBy(s => s.Description);
+                    userModel.Repositories = repos;
+                    break;
+                case "description_desc":
+                    repos = userReposList.OrderByDescending(s => s.Description);
+                    userModel.Repositories = repos;
+                    break;
+                case "stars":
+                    repos = userReposList.OrderBy(s => s.StargazersCount);
+                    userModel.Repositories = repos;
+                    break;
+                case "stars_desc":
+                default:
+                    repos = userReposList.OrderByDescending(s => s.StargazersCount);
+                    userModel.Repositories = repos;
+                    break;
+            }
+
+            return View("Index", userModel);
         }
     }
 }
